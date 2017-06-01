27f56dd0571fd3a4b755a12d905c9e46108f085b
gujarats <gujarat.santana@gmail.com>
2017-06-01 18:07:04 +0700

    first commit

---
+++ app.go
@@ -0,0 +1,186 @@
+package main
+
+import (
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"sync"
+	"time"
+
+	"github.com/urfave/cli"
+)
+
+var wg sync.WaitGroup
+var dir = "files/"
+
+// contants for min and max int
+const MaxUint = ^uint(0)
+const MinUint = 0
+const MaxInt = int(MaxUint >> 1)
+const MinInt = -MaxInt - 1
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "MyCli"
+	app.Usage = "To check csv files"
+	app.Version = "1.0.0"
+	app.Commands = []cli.Command{
+
+		//first command load column data
+		{
+			Name:    "column",
+			Aliases: []string{"c"},
+			Usage:   "Show load result",
+			Action: func(c *cli.Context) error {
+				inputColumn := c.Args().Get(0)
+				if inputColumn == "" {
+					fmt.Println("Please input one column name")
+					return nil
+				}
+
+				csvReader(inputColumn)
+
+				return nil
+			},
+		},
+	}
+
+	app.Run(os.Args)
+
+}
+
+func csvReader(inputColumn string) {
+	startTime := time.Now()
+
+	// get all the files from the specific folder
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	noColumn := make(chan struct{}, len(files))
+	results := make(chan map[string]bool, len(files))
+
+	// reading all the files from dir concurrently
+	for _, file := range files {
+		wg.Add(1)
+		// and read it concurrently to get the data from specific column
+		go func(file os.FileInfo) {
+			defer wg.Done()
+			f, err := os.Open(dir + file.Name())
+			if err != nil {
+				log.Fatal(err)
+			}
+			datas, ok := readFile(inputColumn, f)
+
+			if !ok {
+				noColumn <- struct{}{}
+				return
+			}
+
+			results <- datas
+
+		}(file)
+
+	}
+
+	wg.Wait()
+
+	// check if we got the column or not
+	select {
+	case <-noColumn:
+		fmt.Printf("The column name = %v doesnt exist\n", inputColumn)
+		return
+
+	default: // do nothing and continue
+	}
+
+	close(results)
+	close(noColumn)
+
+	// receive results and determine which size datas is the smallest
+	var datas = make([]map[string]bool, len(results))
+	minIndex := -1
+	minSize := int(MaxUint >> 1)
+	i := 0
+	for values := range results {
+		sizeValues := len(values)
+		if sizeValues < minSize && sizeValues > 0 {
+			minSize = sizeValues
+			minIndex = i
+		}
+		datas[i] = values
+		i++
+	}
+
+	// getting the same value from all the datas
+	var theSameValue []string
+	for value, _ := range datas[minIndex] {
+		isExistAll := false
+		for _, data := range datas {
+			if data[value] {
+				isExistAll = true
+			} else {
+				isExistAll = false
+			}
+		}
+
+		if isExistAll {
+			theSameValue = append(theSameValue, value)
+		}
+	}
+	fmt.Printf("final result = %+v\n", theSameValue)
+	fmt.Printf("final result size = %+v\n", len(theSameValue))
+	fmt.Printf("time consume = %+v\n", time.Since(startTime).Seconds())
+
+}
+
+func readFile(inputColumn string, f *os.File) (map[string]bool, bool) {
+	r := csv.NewReader(bufio.NewReader(f))
+
+	columnIndex := -1
+	datas := make(map[string]bool)
+	isColumnExist := false
+	for {
+		records, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if len(records) > 0 {
+			//find index column
+			if columnIndex == -1 {
+				for index, record := range records {
+					if record == inputColumn {
+
+						isColumnExist = true
+						columnIndex = index
+						break
+					}
+				}
+
+				// avoid inserting the index column
+				continue
+			}
+
+			// get all the values from the column index to ourmap
+			if columnIndex >= 0 {
+				datas[records[columnIndex]] = true
+			} else {
+				break // we didn't finf the column name; exit loop
+			}
+
+		}
+
+	}
+
+	return datas, isColumnExist
+
+}
